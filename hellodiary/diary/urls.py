67a36a1c1f35917b3c4255c61b6a2bf622175878
Kiran <kiranjanardhanan2000@gmail.com>
2021-04-03 00:12:36 +0530

    cleaned the code and added create diary functionality

--- hellodiary/diary/urls.py
+++ hellodiary/diary/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.loginPage, name="loginPage"),
-    path('logout/', views.logoutUser, name="logoutPage"),
+    path('', views.login_page, name="login_page"),
+    path('logout/', views.logout_user, name="logout_page"),
     path('register/', views.register, name="register"),
     path('diary/', views.home, name="home"),
-    path('allEntries/', views.allEntries, name="all-entries"),
-    path('analytics/', views.analytics, name="analytics")
+    path('all_entries/', views.all_entries, name="all_entries"),
+    path('analytics/', views.analytics, name="analytics"),
+    path('create_diary/', views.create_diary, name="create_diary")
 
 ]
